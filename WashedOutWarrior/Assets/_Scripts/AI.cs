18ca8161396620ce0f82bb0746ca8c9258877186
Bereim <35741860+Bereim@users.noreply.github.com>
2018-04-06 13:52:44 -0500

    AI Changes
    ~ Untested

--- WashedOutWarrior/Assets/_Scripts/AI.cs
+++ WashedOutWarrior/Assets/_Scripts/AI.cs
@@ -4,49 +4,42 @@ using UnityEngine;
 
 public class AI : MonoBehaviour {
 	public List<Vector2> nodes = new List<Vector2>();
-	public float speed = 1f, jumpStrength = 1000f, cooldown = 1f;
+	public float speed = 1f, jumpStrength = 1000f;
+    private bool movingLeft = true, isGrounded = false;
 
-	private bool isGrounded = false;
-	private float curCooldown = 0;
-
-	// Use this for initialization
-	void Start () {
-		if (speed > 0) {
-			speed *= -1;
-			Flip();
-		}
-
-		InvokeRepeating("Jump", 1f, 2f);
-	}
-
+    // Use this for initialization
+    void Start () {
+        if (movingLeft) {
+            InvokeRepeating("JumpLeft", 5f, 5f);
+        }
+        else {
+            InvokeRepeating("JumpRight", 5f, 5f);
+        }
+    }
+    
 	private void FixedUpdate() {
-		Collider2D[] collisions = Physics2D.OverlapCircleAll(transform.position, 10f, 9);
+        //See if player is within 10f units
+        Collider2D[] collisions = Physics2D.OverlapCircleAll(transform.position, 10f, 9);
 		foreach (Collider2D collision in collisions) {
 			if (collision.gameObject.CompareTag("Player")) {
-				Vector2 temp = collision.gameObject.transform.position - transform.position;
-				if ((temp.x < 0 && speed > 0) || (temp.x > 0 && speed < 0)) {
-					Flip();
-				}
+                //Difference between this AI and the Player
+                Vector2 temp = collision.gameObject.transform.position - transform.position;
+                //Move AI in right direction
+                if (temp.x < 0 && IsInvoking("JumpRight")) {
+                    CancelInvoke("JumpRight");
+                    InvokeRepeating("JumpLeft", 1f, 5f);
+                }
+                if (temp.x > 0 && IsInvoking("JumpLeft")) {
+                    CancelInvoke("JumpLeft");
+                    InvokeRepeating("JumpRight", 1f, 5f);
+                }
 			}
 		}
-	}
-
-	// Update is called once per frame
-	void Update () {
-		if (curCooldown > 0) curCooldown -= Time.deltaTime;
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.collider.CompareTag("Ground")) {
 			isGrounded = true;
-		}
-	}
-	private void OnCollisionStay2D(Collision2D collision) {
-		if (collision.collider.CompareTag("Player")) {
-			if (curCooldown <= 0) {
-				Jump(new Vector2(-2,0));
-				curCooldown += cooldown;
-			}
 		}
 	}
 	private void OnCollisionExit2D(Collision2D collision) {
@@ -55,19 +48,11 @@ public class AI : MonoBehaviour {
 		}
 	}
 
-	private void Jump() { Jump(Vector2.zero); }
-	private void Jump(Vector2 offset) {
+	private void JumpLeft() { Jump(new Vector2(-1,1)); }
+    private void JumpRight() { Jump(Vector2.one); }
+    private void Jump(Vector2 direction) {
 		if (isGrounded) {
-			GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(jumpStrength, speed * jumpStrength) - offset);
+			GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(direction.x * jumpStrength, direction.y * speed * jumpStrength));
 		}
-	}
-
-	void Flip() {
-		speed *= -1;
-		/*
-		Vector2 localScale = gameObject.transform.localScale;
-		localScale.x *= -1;
-		transform.localScale = localScale;
-		*/
 	}
 }
---
+++ WashedOutWarrior/Assets/_Scripts/CopyTransform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyTransform : MonoBehaviour {
+    public GameObject target;
+	public Vector3 posOffset, rotOffset;
+
+	void Update () {
+        transform.position = target.transform.position + posOffset;
+        transform.rotation = target.transform.rotation * Quaternion.Euler(rotOffset);
+	}
+}
