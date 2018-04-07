463fcf13d1c70288093980dd9fec0a4e64f526a4
Mack Peters <mpeters@harding.edu>
2018-04-07 10:35:42 -0500

    Started work on main menu
    +Made background
    +Implemented background scroll

---
+++ WashedOutWarrior/Assets/_Scripts/BGScroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//code from https://unity3d.com/learn/tutorials/topics/2d-game-creation/2d-scrolling-backgrounds
+public class BGScroller : MonoBehaviour
+{
+	public float scrollSpeed;
+	public float tileSizeZ;
+
+	private Vector3 startPosition;
+
+	void Start ()
+	{
+		startPosition = transform.position;
+	}
+
+	void Update ()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + Vector3.down * newPosition;
+	}
+}
