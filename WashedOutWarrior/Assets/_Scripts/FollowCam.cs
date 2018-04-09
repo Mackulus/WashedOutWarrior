eb3cf3d6173f2475205b80511e2d9bca3f8cc77d
Bereim <35741860+Bereim@users.noreply.github.com>
2018-04-08 19:47:34 -0500

    Finished Weapon Swing
    + Weapon swing perfected

--- WashedOutWarrior/Assets/_Scripts/FollowCam.cs
+++ WashedOutWarrior/Assets/_Scripts/FollowCam.cs
@@ -4,10 +4,12 @@ public class FollowCam : MonoBehaviour {
     public GameObject target;
 	public Vector3 offset;
 
-	void Update () {
-        this.transform.position = target.transform.position + offset;
+	private void FixedUpdate() {
+		this.transform.position = target.transform.position + offset;
+	}
 
-		if (Input.GetAxis("Vertical") < 0.0F) {
+	void Update () {
+        if (Input.GetAxis("Vertical") < 0.0F) {
 			transform.GetChild(0).localPosition = Vector3.zero;
 		}
 		else transform.GetChild(0).localPosition = new Vector2(0, 10);
--- WashedOutWarrior/Assets/_Scripts/PlayerController.cs
+++ WashedOutWarrior/Assets/_Scripts/PlayerController.cs
@@ -25,6 +25,17 @@ public class PlayerController : Listener {
 		}
 	}
 
+	private void FixedUpdate() {
+		if (IsFacingLeft()) {
+			weapon.posOffset = Vector3.back;
+			weapon.rotOffset = Vector3.zero;
+		}
+		else {
+			weapon.posOffset = new Vector3(1, -0.25f, -1);
+			weapon.rotOffset = new Vector3(0, 0, -125);
+		}
+	}
+
 	private void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.collider.CompareTag("Enemy")) {
 			healthBar.OnDamage();
@@ -42,21 +53,35 @@ public class PlayerController : Listener {
 	public IEnumerator SwingWeapon() {
 		isSwinging = true;
 		weapon.transform.GetChild(0).tag = "Weapon";
-		print("SwingWeapon");
-		int n = 0;
-		float swingIncrement = 7.5f;
-		while (curWeaponAngle < maxWeaponAngle) {
-			yield return new WaitForFixedUpdate();
-			Vector3 tempVec = weapon.rotOffset;
-			tempVec += new Vector3(0,0, swingIncrement);
-			curWeaponAngle += swingIncrement;
-			n++;
-			weapon.rotOffset = tempVec;
+		//print("SwingWeapon");
+
+		int nTimes = 20;
+		float startOffset, endOffset;
+		if (IsFacingLeft()) {
+			//Swing in a 140 degree left-facing arc
+			startOffset = 0f;
+			endOffset = 140f;
+			for(int ix = (int)startOffset; ix <= endOffset; ix += 140 / nTimes) {
+				yield return new WaitForFixedUpdate();
+				weapon.rotOffset = Vector3.forward * ix;
+			}
 		}
-		weapon.rotOffset -= new Vector3(0, 0, n * swingIncrement);
-		curWeaponAngle = 0;
+		else {
+			//Swing in a 140 degree right-facing arc
+			startOffset = -125f;
+			endOffset = -265f;
+			for (int ix = (int)startOffset; ix >= endOffset; ix -= 140 / nTimes) {
+				yield return new WaitForFixedUpdate();
+				weapon.rotOffset = Vector3.forward * ix;
+			}
+		}
+		
 		weapon.transform.GetChild(0).tag = "Untagged";
 		isSwinging = false;
+	}
+
+	private bool IsFacingLeft() {
+		return gordo.transform.localScale.x > 0;
 	}
 
 	override public void OnHear() {
