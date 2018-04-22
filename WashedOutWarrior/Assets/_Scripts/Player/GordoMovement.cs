dec588f1b3988c6674394cfd421b61d0d78643d1
Mack Peters <mpeters@harding.edu>
2018-04-22 10:41:28 -0500

    Made jump a little smoother
    +Added a landing animation
    +A box collider to Gordo's lower half and took off the feet colliders because they were messing with the isGrounded property
    ~Moved scripts into their own folders under titles that I believe are relevant to their functions

--- WashedOutWarrior/Assets/_Scripts/Player/GordoMovement.cs
+++ WashedOutWarrior/Assets/_Scripts/Player/GordoMovement.cs
@@ -26,12 +26,12 @@ public class GordoMovement : MonoBehaviour {
     void MoveGordo() {
 		moveX = Input.GetAxis("Horizontal");
 		moveY = Input.GetAxis("Vertical");
-		if (moveX != 0.0f && isWalking == false && moveY == 0.0f) {
+		if (moveX != 0.0f && isWalking == false && isGrounded) {
 			anim.ResetTrigger("Idle_01");
 			anim.SetTrigger("Walk_01");
 			isWalking = true;
 		}
-		else if (moveX == 0.0f && isWalking == true && moveY == 0.0f) {
+		else if (moveX == 0.0f && isWalking == true && isGrounded) {
 			anim.ResetTrigger("Walk_01");
 			anim.SetTrigger("Idle_01");
 			isWalking = false;
@@ -62,7 +62,7 @@ public class GordoMovement : MonoBehaviour {
             gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
 		    anim.SetTrigger("Jump_01");
 		    Invoke("GordoJumpForce", 0.25f);
-		    Invoke("TurnJumpOff", 1.22f);
+			StartCoroutine(WaitForTakeOff());
         }
 	}
 
@@ -70,14 +70,28 @@ public class GordoMovement : MonoBehaviour {
 		gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up*gordoJumpPower);
 	}
 
-	void TurnJumpOff() {
+	public IEnumerator WaitForTakeOff() {
+		yield return new WaitUntil(()=> isGrounded == false);
+		StartCoroutine(WaitForGrounded());
+	}
+
+	public IEnumerator WaitForGrounded() {
+		yield return new WaitUntil(()=> isGrounded == true);
+		print("Made it to the ground");
+		anim.SetTrigger("Land_01");
+		Invoke("FinishJump", 0.5f);
+	}
+
+	void FinishJump()
+	{
 		isJumping = false;
+		isWalking = false;
 	}
 
     private void OnCollisionEnter2D(Collision2D collision) {
         //print(collision.collider.tag);
 		if (collision.collider.CompareTag("Ground") || collision.collider.CompareTag("Enemy") || collision.collider.CompareTag("LollipopWall")) {
-			//print("Entered");
+			print("Entered");
             isGrounded = true;
         }
     }
@@ -88,7 +102,7 @@ public class GordoMovement : MonoBehaviour {
 	}
 	private void OnCollisionExit2D(Collision2D collision) {
 		if (collision.collider.CompareTag("Ground") || collision.collider.CompareTag("Enemy") || collision.collider.CompareTag("LollipopWall")) {
-			//print("Exited");
+			print("Exited");
 			isGrounded = false;
 		}
     }
