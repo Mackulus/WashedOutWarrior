9814ec4e44bc9ae55c63ed5b8bb7c7620558ce7a
Bereim <35741860+Bereim@users.noreply.github.com>
2018-04-22 19:17:02 -0500

    Removed Error caused by AIDuplicator on Level Fail

--- WashedOutWarrior/Assets/_Scripts/AI/AIDuplicator.cs
+++ WashedOutWarrior/Assets/_Scripts/AI/AIDuplicator.cs
@@ -6,47 +6,31 @@ public class AIDuplicator : MonoBehaviour {
 	public AISensors sensor;
 	public GameObject toDuplicate;
 	private bool duplicate = true;
-	private bool isQuitting = false;
 
-	void OnApplicationQuit() {
-		isQuitting = true;
-	}
-
-	void OnDisable() {
-		if (duplicate && !isQuitting && toDuplicate != null) {
-			for (int i = 0; i < 2; i++) {
-				GameObject temp = Instantiate(toDuplicate);
-				if (transform != null) {
-					temp.transform.position = transform.position;
-				}
-				if (sensor != null) {
-					temp.transform.position += (Vector3)(sensor.playerRelPos * -2);
-				}
-
-				/*
-				Vector2 parentPosition = transform.localPosition;
-				GameObject clone = Instantiate(toDuplicate.gameObject);
-				
-				clone.transform.localPosition = new Vector2 (parentPosition.x, parentPosition.y + 5 + i);
-				clone.transform.localScale = new Vector3(transform.localScale.x / 1.5f , transform.localScale.y / 1.5f, transform.localScale.z / 1.5f);
-				//Currently is specific to pizza because there is no good way to loop through components and check if they're disabled
-
-				clone.GetComponent<BoxCollider2D>().enabled = true;
-				clone.GetComponent<AISensors>().enabled = true;
-				clone.GetComponent<AIMovement>().enabled = true;
-				clone.GetComponent<AIDuplicator>().ChangeDuplicate(false);
-				clone.GetComponentInChildren<HealthBar>().enabled = true;
-				clone.GetComponentInChildren<HealthBar>().maxHealth = toDuplicate.healthBar.maxHealth/2;
-				clone.GetComponentInChildren<HealthBar>().damage = 0;
-				clone.SetActive(true);
-				*/
+	void Duplicate() {
+		for (int i = 0; i < 2; i++) {
+			GameObject temp = Instantiate(toDuplicate);
+			if (transform != null) {
+				temp.transform.position = transform.position;
+			}
+			if (sensor != null) {
+				temp.transform.position += (Vector3)(sensor.playerRelPos * -2);
 			}
 		}
-		print("Pizza was destroyed");
 	}
 
 	//To be used later to make a boss where some duplications are false and others are true;
 	void ChangeDuplicate(bool newDuplicate) {
 		duplicate = newDuplicate;
+	}	
+	
+	void OnDisable() {
+		if (toDuplicate != null) {
+			Invoke("Duplicate", 0.01f);
+		}
+	}
+
+	private void OnDestroy() {
+		CancelInvoke();
 	}
 }
