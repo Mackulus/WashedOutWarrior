87ccde331c9ee4711b629144e0e65cbeb64f33e6
Bereim <35741860+Bereim@users.noreply.github.com>
2018-04-22 19:31:32 -0500

    Reenabled looking down
    + Made it pretty too

--- WashedOutWarrior/Assets/_Scripts/Camera&Backgrounds/FollowCam.cs
+++ WashedOutWarrior/Assets/_Scripts/Camera&Backgrounds/FollowCam.cs
@@ -8,13 +8,14 @@ public class FollowCam : MonoBehaviour {
 	private void LateUpdate() {
 		float x = Mathf.Clamp(target.transform.position.x, xMin, xMax);
 		float y = Mathf.Clamp(target.transform.position.y, yMin, yMax);
+		//this.transform.position = Vector3.Lerp(transform.position, new Vector3(x, y, target.transform.position.z + offset.z), 0.05f);
 		this.transform.position = new Vector3(x, y, target.transform.position.z + offset.z);
 	}
 
-	//void Update () {
-    //    if (Input.GetAxis("Vertical") < 0.0F) {
-	//		transform.GetChild(0).localPosition = Vector3.zero;
-	//	}
-	//	else transform.GetChild(0).localPosition = new Vector2(0, 10);
-	//}
+	void Update() {
+		if (Input.GetAxis("Vertical") < 0.0F) {
+			transform.GetChild(0).localPosition = Vector3.Lerp(transform.GetChild(0).localPosition, Vector3.zero, 0.05f);
+		}
+		else transform.GetChild(0).localPosition = Vector3.Lerp(transform.GetChild(0).localPosition, new Vector2(0, 10), 0.05f);
+	}
 }
